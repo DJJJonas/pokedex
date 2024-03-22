d4b9c73710fc33e2e22c567834f8e31746da1d0a
DJJJonas <jonasmelo@duck.com>
2024-03-21 23:23:16 -0300

    refactor: use dialog tag for modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,19 +1,7 @@
 export default class Modal {
   static setupEvents() {
-    // Make the modal not close if it's content is clicked
-    modal.onclick = (e) => e.stopPropagation();
-
-    modalContainer.onclick = () => {
-      modalContainer.animate([{ opacity: 1 }, { opacity: 0 }], {
-        duration: 100,
-      }).onfinish = () => {
-        this.toggle();
-      };
-    };
-
-    pkmSprite.onclick = () => {
-      this.toggle();
-    };
+    pkmSprite.onclick = () => modal.showModal();
+    modal.onclick = () => modal.close();
   }
 
   /**
